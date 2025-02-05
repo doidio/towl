727b22eb95f104f0993e1981370c3f4aeddf44da
yiping.wang <yiping.wang@midivi.cn>
2025-02-05 11:34:51 +0800

    update

--- towl/wp.py
+++ towl/wp.py
@@ -73,6 +73,28 @@ def volume_ray_parallel(
 
 
 @wp.kernel
+def volume_slice(
+        volume: wp.uint64, volume_origin: wp.vec3, volume_spacing: wp.vec3,
+        image: wp.array(dtype=wp.uint8, ndim=2), image_origin: wp.vec3, image_iso_spacing: float,
+        image_x_axis: wp.vec3, image_y_axis: wp.vec3,
+        window_min: float, window_max: float,
+):
+    i, j = wp.tid()
+
+    position = image_origin
+    position += float(i) * image_iso_spacing * image_x_axis
+    position += float(j) * image_iso_spacing * image_y_axis
+
+    uvw = wp.cw_div(position - volume_origin, volume_spacing)
+    pixel = wp.volume_sample_f(volume, uvw, wp.Volume.LINEAR)
+
+    pixel = wp.round((pixel - window_min) / (window_max - window_min) * 255.0)
+    pixel = wp.clamp(pixel, 0.0, 255.0)
+
+    image[i, j] = wp.uint8(pixel)
+
+
+@wp.kernel
 def mesh_ray_parallel(
         mesh: wp.uint64, base_color: wp.vec3,
         image: wp.array(dtype=wp.vec3, ndim=2), image_origin: wp.vec3, image_iso_spacing: float,
@@ -152,25 +174,3 @@ def mesh_slice(
                 mesh, position, mesh_max_dist, wp.float32(1e-3),
         ).sign < 0:
             image[i, j] = opacity * base_color * 255.0 + (1.0 - opacity) * image[i, j]
-
-
-@wp.kernel
-def volume_slice(
-        volume: wp.uint64, volume_origin: wp.vec3, volume_spacing: wp.vec3,
-        image: wp.array(dtype=wp.uint8, ndim=2), image_origin: wp.vec3, image_iso_spacing: float,
-        image_x_axis: wp.vec3, image_y_axis: wp.vec3,
-        window_min: float, window_max: float,
-):
-    i, j = wp.tid()
-
-    position = image_origin
-    position += float(i) * image_iso_spacing * image_x_axis
-    position += float(j) * image_iso_spacing * image_y_axis
-
-    uvw = wp.cw_div(position - volume_origin, volume_spacing)
-    pixel = wp.volume_sample_f(volume, uvw, wp.Volume.LINEAR)
-
-    pixel = wp.round((pixel - window_min) / (window_max - window_min) * 255.0)
-    pixel = wp.clamp(pixel, 0.0, 255.0)
-
-    image[i, j] = wp.uint8(pixel)
