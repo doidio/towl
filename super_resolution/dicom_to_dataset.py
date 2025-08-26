73b58314cec109fbd9994461b25299f83e15ba3f
epw <88172828@qq.com>
2025-08-26 17:15:46 +0800

    增加了全身图子区域滑窗式特征匹配，训练改为双侧拼接图，增加了窗对比度退化

--- super_resolution/dicom_to_dataset.py
+++ super_resolution/dicom_to_dataset.py
@@ -1,15 +1,16 @@
-# pip install numpy chainner-ext pillow pydicom tqdm
+# pip install numpy opencv-python chainner-ext pillow pydicom tqdm
 
 # https://github.com/neosr-project/neosr/wiki/Installation-Instructions#manual-installation
 # cd neosr
-# uv run --isolated train.py -opt ../super_resolution/train.toml
+# uv run --isolated train.py -opt ../super_resolution/train_8x.toml
+# uv run --isolated test.py -opt ../super_resolution/test_8x.toml
 
 import argparse
 import warnings
 import zipfile
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
-from random import choice
+from random import choice, random
 
 import cv2
 import numpy as np
@@ -32,84 +33,131 @@ def main(zip_file: str, dataset_dir: str, is_val: bool):
     subfolder = 'val' if is_val else 'train'
     gt_dir = dataset_dir / subfolder / 'gt'
     lq_dir = dataset_dir / subfolder / 'lq'
+    test_dir = dataset_dir / 'test'
 
     with zipfile.ZipFile(zip_file) as zf:
-        for file in zf.namelist():
-            with zf.open(file) as f:
-                try:
-                    ds = pydicom.dcmread(f)
-                except InvalidDicomError:
-                    continue
 
-                if ds.BitsAllocated != 8:  # 仅支持 8-bit，若支持 16-bit 需相应变更超分训练配置
-                    warnings.warn(f'{zip_file} {file} 8-bit ≠ {ds.BitsAllocated}')
-                    continue
+        def find(protocols):
+            for file in zf.namelist():
+                with zf.open(file) as f:
+                    try:
+                        assert (it := pydicom.dcmread(f)).pixel_array
+                    except (InvalidDicomError, ValueError):
+                        continue
 
-                if ds.PhotometricInterpretation != 'MONOCHROME2':  # 仅支持白色前景黑色背景
-                    warnings.warn(f'{zip_file} {file} MONOCHROME2 ≠ {ds.PhotometricInterpretation}')
-                    continue
+                    if it.BitsAllocated != 8:  # 仅支持 8-bit，若支持 16-bit 需相应变更超分训练配置
+                        warnings.warn(f'{zip_file} {file} 8-bit ≠ {it.BitsAllocated}')
+                        continue
 
-                # 转换髋膝局部图
-                if (ProtocolName := ds.ProtocolName.strip().replace(' ', '_')) in [
-                    'Left_Femur', 'Right_Femur',
-                    'Left_Ortho Knee', 'Right_Ortho_Knee',
-                ]:
-                    # GT图16倍降采样之后的LQ图，是16的整数倍，避免tile推理
-                    base = 16 * 16
-                    h, w = ds.pixel_array.shape[:2]
+                    if it.PhotometricInterpretation != 'MONOCHROME2':  # 仅支持白色前景黑色背景
+                        warnings.warn(f'{zip_file} {file} MONOCHROME2 ≠ {it.PhotometricInterpretation}')
+                        continue
 
-                    pad_h = (base - h % base) % base
-                    pad_w = (base - w % base) % base
+                    if it.ProtocolName in protocols:
+                        yield it
 
-                    top, bottom = pad_h // 2, pad_h - pad_h // 2
-                    left, right = pad_w // 2, pad_w - pad_w // 2
+        # test
+        for ds in find(('Total Body',)):
+            base = 16
+            h, w = ds.pixel_array.shape[:2]
 
-                    gt_image = cv2.copyMakeBorder(ds.pixel_array, top, bottom, left, right, cv2.BORDER_REFLECT101)
+            pad_h = (base - h % base) % base
+            pad_w = (base - w % base) % base
 
-                    # 退化
-                    lq_image = gt_image.astype(np.float32) / 255.0
-                    h, w = lq_image.shape[:2]
+            image = cv2.copyMakeBorder(ds.pixel_array, 0, pad_h, 0, pad_w, cv2.BORDER_REPLICATE)
 
+            _ = test_dir / 'TotalBody' / f'{Path(zip_file).stem}_TotalBody_{ds.InstanceNumber}.png'
+            _.parent.mkdir(parents=True, exist_ok=True)
+            Image.fromarray(image, mode='L').convert('RGB').save(_)
+
+        # train & val
+        for roi in ('Femur', 'Ortho Knee'):
+            roi_ds = []
+            for i in ('Right', 'Left'):
+                if len(ds := list(find(' '.join([i, roi])))) > 0:
+                    roi_ds.append(ds[0])
+
+            if len(roi_ds) != 2:
+                continue
+
+            wl = random() * 0.5 + 0.5, random() * 0.5 + 0.25
+            rf = choice([ResizeFilter.Box, ResizeFilter.Linear, ResizeFilter.Lagrange, ResizeFilter.Gauss])
+
+            gt_images, lq_images = [], []
+            for ds in roi_ds:
+                # GT图scaling倍降采样之后的LQ图，是16的整数倍，避免tile推理
+                base = 16 * 16
+                h, w = ds.pixel_array.shape[:2]
+
+                pad_h = (base - h % base) % base
+                pad_w = (base - w % base) % base
+
+                top, bottom = pad_h // 2, pad_h - pad_h // 2
+                left, right = pad_w // 2, pad_w - pad_w // 2
+
+                gt_image = cv2.copyMakeBorder(ds.pixel_array, top, bottom, left, right, cv2.BORDER_REFLECT101)
+                gt_images.append(gt_image)
+
+                # 退化
+                lq_image = gt_image.astype(np.float32) / 255.0
+                h, w = lq_image.shape[:2]
+
+                ksize = 7
+                for scaling in (2, 4, 8,):
                     # 模糊
-                    ksize = 7
                     lq_image = cv2.GaussianBlur(lq_image, (ksize, ksize), 0)
 
                     # 降采样
-                    rf = choice([ResizeFilter.Box, ResizeFilter.Linear, ResizeFilter.Lagrange, ResizeFilter.Gauss])
-                    lq_image = resize(lq_image, (w // 16, h // 16), rf, gamma_correction=True)
-                    lq_image = (np.clip(lq_image, 0.0, 1.0) * 255.0).astype(np.uint8).squeeze()
+                    lq_image = resize(lq_image, (w // scaling, h // scaling), rf, gamma_correction=True)
 
-                    # 保存LQ图
-                    _ = lq_dir / f'{Path(zip_file).stem}_{ProtocolName}.png'
-                    _.parent.mkdir(parents=True, exist_ok=True)
-                    Image.fromarray(lq_image, mode='L').convert('RGB').save(_)
+                # 降低对比度，全身窗与局部窗不同，TODO
+                lq_image = ((lq_image - wl[1]) / wl[0] + 1) / 2
 
-                    # 保存GT图
-                    _ = gt_dir / f'{Path(zip_file).stem}_{ProtocolName}.png'
-                    _.parent.mkdir(parents=True, exist_ok=True)
-                    Image.fromarray(gt_image, mode='L').convert('RGB').save(_)
+                lq_image = (np.clip(lq_image, 0.0, 1.0) * 255.0).astype(np.uint8).squeeze()
+                lq_images.append(lq_image)
+
+            # 拼接
+            def stack(arr: list):
+                shape = max(arr[0].shape[0], arr[1].shape[0]), arr[0].shape[1] + arr[1].shape[1]
+                stacked = np.zeros(shape, dtype=arr[0].dtype)
+                stacked[:arr[0].shape[0], :arr[0].shape[1]] = arr[0]
+                stacked[:arr[1].shape[0], arr[0].shape[1]:arr[0].shape[1] + arr[1].shape[1]] = arr[1]
+                return stacked
+
+            roi = roi.replace(' ', '')
+
+            # 保存LQ
+            _ = lq_dir / '8x' / f'{Path(zip_file).stem}_{roi}.png'
+            _.parent.mkdir(parents=True, exist_ok=True)
+            Image.fromarray(stack(lq_images), mode='L').convert('RGB').save(_)
+
+            # 保存GT
+            _ = gt_dir / f'{Path(zip_file).stem}_{roi}.png'
+            _.parent.mkdir(parents=True, exist_ok=True)
+            Image.fromarray(stack(gt_images), mode='L').convert('RGB').save(_)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--zip_dir', required=True)
-    parser.add_argument('--dataset_dir', required=True)
-    parser.add_argument('--max_workers', default=16)
+    parser.add_argument('--zip_dir', type=str, required=True)
+    parser.add_argument('--dataset_dir', type=str, required=True)
+    parser.add_argument('--val_count', type=int, default=100)
+    parser.add_argument('--max_workers', type=int, default=16)
     args = parser.parse_args()
 
     zip_dir = Path(args.zip_dir).absolute()
     zip_files = [_ for _ in zip_dir.rglob('*.zip')]
 
     bools = np.zeros(len(zip_files), bool)
-    bools[::1000] = True
-
-    items = list(zip(zip_files, bools))
+    bools[:args.val_count] = True
 
     with ProcessPoolExecutor(max_workers=args.max_workers) as executor:
-        futures = {executor.submit(main, it[0].as_posix(), args.dataset_dir, it[1]) for it in items}
+        futures = {executor.submit(
+            main, it[0].as_posix(), args.dataset_dir, it[1],
+        ) for it in zip(zip_files, bools)}
 
         try:
-            for _ in tqdm(as_completed(futures), total=len(futures), unit='it', unit_scale=True, leave=True):
+            for _ in tqdm(as_completed(futures)):
                 try:
                     _.result()
                 except Exception as e:
---
+++ super_resolution/feature_match.py
@@ -0,0 +1,160 @@
+# PyTorch
+# pip install numpy PIL opencv-python kornia tqdm
+
+import argparse
+import colorsys
+from concurrent.futures import as_completed, ThreadPoolExecutor
+from pathlib import Path
+from random import randint
+
+import cv2
+import kornia
+import numpy as np
+import torch
+from PIL import Image
+from tqdm import tqdm
+
+
+def to_tensor(img):
+    t = torch.from_numpy(np.array(img)).float() / 255.
+    t = t.permute(2, 0, 1).unsqueeze(0)  # [B,C,H,W]
+    return t
+
+
+def feature_match(matcher, conf_sums, image_0, image_1, h, crop_h):
+    t1 = to_tensor(image_0[h:h + crop_h]).cuda()
+    t2 = to_tensor(image_1).cuda()
+
+    with torch.no_grad():
+        out = matcher({
+            'image0': kornia.color.rgb_to_grayscale(t1),
+            'image1': kornia.color.rgb_to_grayscale(t2),
+        })
+
+    kp0 = out['keypoints0'].cpu().numpy()
+    kp1 = out['keypoints1'].cpu().numpy()
+    conf = out['confidence'].cpu().numpy()
+    conf_sums[h] = np.sum(conf)
+    return h, kp0, kp1, conf
+
+
+def draw_matches(im0, im1, kp0, kp1, conf):
+    h0, w0 = im0.shape[:2]
+    h1, w1 = im1.shape[:2]
+
+    canvas_h = h0 + h1
+    canvas_w = max(w0, w1)
+
+    # 创建画布
+    canvas = np.zeros((canvas_h, canvas_w, 3), dtype=np.uint8)
+
+    # 计算左右偏移量，使两张图都居中
+    offset0 = (canvas_w - w0) // 2
+    offset1 = (canvas_w - w1) // 2
+
+    # 分别粘贴 im0 和 im1（上下排列，水平居中）
+    canvas[:h0, offset0:offset0 + w0] = im0
+    canvas[h0:h0 + h1, offset1:offset1 + w1] = im1
+
+    # 绘制匹配
+    for (x0, y0), (x1, y1), c in zip(kp0.astype(int), kp1.astype(int), conf.astype(float)):
+        r, g, b = colorsys.hsv_to_rgb(randint(0, 12) * 30 / 360, 1, 1)
+        # r, g, b = colorsys.hsv_to_rgb(c * 120 / 360, 1, 1)
+        color = tuple(int(x * 255) for x in (b, g, r))
+
+        cv2.circle(canvas, (x0 + offset0, y0), 3, color, -1)  # im0 的点
+        cv2.circle(canvas, (x1 + offset1, y1 + h0), 3, color, -1)  # im1 的点（再加垂直偏移 h0）
+        cv2.line(canvas, (x0 + offset0, y0), (x1 + offset1, y1 + h0), color, 1)
+    return canvas
+
+
+def main(dataset_dir: str, TotalBody_file: str, roi_files: dict, matcher: kornia.feature.LoFTR):
+    TotalBody_file = Path(TotalBody_file)
+    if (touch := TotalBody_file.parent.parent / 'TotalBody_Matched' / TotalBody_file.name).exists():
+        return
+
+    dataset_dir = Path(dataset_dir).absolute()
+    test_dir = dataset_dir / 'test'
+
+    _ = TotalBody_file.stem.split('_')
+    prefix, TotalBody_i = '_'.join(_[:-2]), _[-1]
+
+    # 全身图
+    image_0 = np.array(Image.open(TotalBody_file))
+
+    # 局部图
+    roi_files = {roi: roi_files.get(f'{prefix}_{roi}') for roi in ('Femur', 'OrthoKnee')}
+
+    if None not in roi_files.values():
+        for roi in roi_files:
+            image_1 = np.array(Image.open(roi_files[roi]))
+
+            # 特征匹配
+            crop_h = image_1.shape[0]
+            end_h = image_0.shape[0] - crop_h
+
+            conf_sums = np.zeros((end_h, 1), float)
+            kps = {}
+
+            step = 16
+            with ThreadPoolExecutor(max_workers=args.max_workers) as executor:
+                futures = {executor.submit(
+                    feature_match, matcher, conf_sums, image_0, image_1, h, crop_h,
+                ) for h in range(0, end_h, step)}
+
+                for _ in as_completed(futures):
+                    h, kp0, kp1, conf = _.result()
+                    kps[h] = (kp0, kp1, conf)
+
+            conf_sums = conf_sums[::step]
+            for _ in range(5):
+                conf_sums = cv2.blur(conf_sums, (5, 5))
+            conf_sums = conf_sums.squeeze()
+
+            best_h = int(conf_sums.argmax())
+            best_conf_sums = conf_sums[best_h]
+
+            best_h *= step
+            best_kp0, best_kp1, best_conf = kps[best_h]
+
+            lv = int(best_conf_sums // 10 * 10) + 5  # 按置信度分组全身图
+            roi_image = image_0[best_h:best_h + crop_h]
+            canvas = draw_matches(roi_image, image_1, best_kp0, best_kp1, best_conf)
+
+            stem = f'{prefix}_TotalBody_{TotalBody_i}_{roi}_{best_h}_{crop_h}'
+
+            _ = test_dir / '8x_Match' / f'Conf_{lv}' / f'{stem}.png'
+            _.parent.mkdir(parents=True, exist_ok=True)
+            Image.fromarray(canvas).save(_)
+
+            _ = test_dir / '8x' / f'Conf_{lv}' / f'{stem}.png'
+            _.parent.mkdir(parents=True, exist_ok=True)
+            Image.fromarray(roi_image).save(_)
+
+    touch.parent.mkdir(parents=True, exist_ok=True)
+    touch.touch()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_dir', type=str, required=True)
+    parser.add_argument('--initial', type=int, default=0)
+    parser.add_argument('--max_workers', type=int, default=16)
+    args = parser.parse_args()
+
+    _matcher = kornia.feature.LoFTR(pretrained='outdoor').cuda()
+
+    _roi_files = {
+        Path(_).stem: _
+        for __ in ('val', 'train')
+        for _ in (Path(args.dataset_dir) / __ / 'lq' / '8x').rglob('*.png')
+    }
+
+    TotalBody_files = list((Path(args.dataset_dir) / 'test' / 'TotalBody').rglob('*.png'))
+
+    try:
+        for _ in tqdm(TotalBody_files, initial=args.initial):
+            main(args.dataset_dir, _.as_posix(), _roi_files, _matcher)
+
+    except KeyboardInterrupt:
+        print('Interrupted')
