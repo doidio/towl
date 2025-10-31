b3d6087d86588c9c129215eaf0c5eb3eba53d716
epw <88172828@qq.com>
2025-10-31 13:34:35 +0800

    fix

--- totalseg/run_webdav.py
+++ totalseg/run_webdav.py
@@ -21,7 +21,7 @@ def progress(prefix, current, total):
     speed[1] = time.perf_counter()
     _ = float(current) / (speed[1] - speed[0]) if speed[0] != speed[1] else 0.0
     speed[2] = f'{humanize.naturalsize(_, True)}/s'
-    print(f'\r[{prefix}] {100 * current / total:.2f}% {speed[2]}', end='', flush=True)
+    print(f'\r[{prefix}] {speed[2]} {100 * current / total:.2f}%', end='', flush=True)
 
 
 def main(cfg_path: str):
@@ -58,42 +58,44 @@ def main(cfg_path: str):
             cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
             rw.upload_to(b'', done)
 
-            try:
-                seconds = time.perf_counter()
-                with tempfile.TemporaryDirectory() as tdir:
-                    tdir = Path(tdir)
-                    image = tdir / 'image.nii.gz'
+            seconds = time.perf_counter()
+            with tempfile.TemporaryDirectory() as tdir:
+                tdir = Path(tdir)
+                image = tdir / 'image.nii.gz'
 
-                    # 下载
-                    global speed
+                # 下载
+                global speed
+                speed = [time.perf_counter()] * 2 + ['']
+                t = time.perf_counter()
+                ro.download_file(f, image, partial(progress, _ := '下载'))
+                t = time.perf_counter() - t
+                print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
+
+                # 推理
+                for task in cfg['task']:
+                    label = tdir / f'{task}.nii.gz'
+                    ln = cfg['task'][task].get('license_number')
+
+                    print(f'\r[推理] {task}', end='', flush=True)
+                    t = time.perf_counter()
+                    totalsegmentator(image, label, ml=True, task=task, license_number=ln, quiet=True)
+                    t = time.perf_counter() - t
+                    print(f'\r[推理] {task} {t:.2f} 秒', flush=True)
+
+                    # 上传
+                    rw.mkdir(task)
+
                     speed = [time.perf_counter()] * 2 + ['']
-                    ro.download_file(f, image, partial(progress, _ := '下载'))
-                    print(f'\r[{_}] {speed[2]}', flush=True)
-
-                    # 推理
-                    for task in cfg['task']:
-                        label = tdir / f'{task}.nii.gz'
-                        ln = cfg['task'][task].get('license_number')
-
-                        print(f'[推理] {task}')
-                        totalsegmentator(image, label, ml=True, task=task, license_number=ln, quiet=True)
-
-                        # 上传
-                        rw.mkdir(task)
-
-                        speed = [time.perf_counter()] * 2 + ['']
-                        rw.upload_file(_ := f'{task}/{f}', label, partial(progress, _ := '上传'))
-                        print(f'\r[{_}] {speed[2]}', flush=True)
+                    t = time.perf_counter()
+                    rw.upload_file(_ := f'{task}/{f}', label, partial(progress, _ := '上传'))
+                    t = time.perf_counter() - t
+                    print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
 
                 del cfg['undone']
                 cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
 
                 seconds = time.perf_counter() - seconds
-                print(f'[用时] {seconds:.0f} 秒')
-
-            except Exception as e:
-                ro.clean(done)
-                raise e
+                print(f'[用时] {seconds:.0f} 秒\n')
 
             break
 
