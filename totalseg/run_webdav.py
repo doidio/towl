50f22279d9801c8361457e5781beb532f596c28f
epw <88172828@qq.com>
2025-10-31 15:04:04 +0800

    fix

--- totalseg/run_webdav.py
+++ totalseg/run_webdav.py
@@ -2,8 +2,10 @@
 # pip install tomlkit webdavclient3 humanize TotalSegmentator
 
 import argparse
+import sys
 import tempfile
 import time
+import warnings
 from functools import partial
 from pathlib import Path
 
@@ -24,7 +26,7 @@ def progress(prefix, current, total):
     print(f'\r[{prefix}] {speed[2]} {100 * current / total:.2f}%', end='', flush=True)
 
 
-def main(cfg_path: str):
+def main(cfg_path: str, clean: bool):
     cfg_path = Path(cfg_path)
     cfg = tomlkit.loads(cfg_path.read_text('utf-8'))
 
@@ -38,73 +40,79 @@ def main(cfg_path: str):
                 rw.clean(_ := f'.done/{_}')
                 del cfg['undone']
                 cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
-                print(f'[清理] {_}')
-                return
+                print(f'[清理] {_}\n')
 
-    while True:
-        ro_list = ro.list()
-        rw_list = rw.list('.done')
-        print(f'[完成] {len(rw_list)}/{len(ro_list)} {100 * len(rw_list) / len(ro_list):.3f}%')
+    if clean:
+        sys.exit()
 
-        for f in ro.list():
-            done = f'.done/{f}'
+    ro_list = ro.list()
+    rw_list = rw.list('.done')
+    print(f'[完成] {len(rw_list)}/{len(ro_list)} {100 * len(rw_list) / len(ro_list):.3f}%')
 
-            # 跳过完成
-            if requests.head(f'{rw.webdav.hostname}/{done}', auth=(rw.webdav.login, rw.webdav.password)).ok:
-                continue
+    for f in (set(ro_list) - set(rw_list)):
+        done = f'.done/{f}'
 
-            print(f'[新建] {f}')
+        # 跳过完成
+        if requests.head(f'{rw.webdav.hostname}/{done}', auth=(rw.webdav.login, rw.webdav.password)).ok:
+            continue
 
-            # 文件锁
-            cfg['undone'] = f
-            cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
-            rw.upload_to(b'', done)
+        print(f'[新建] {f}')
 
-            seconds = time.perf_counter()
-            with tempfile.TemporaryDirectory() as tdir:
-                tdir = Path(tdir)
-                image = tdir / 'image.nii.gz'
+        # 文件锁
+        cfg['undone'] = f
+        cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
+        rw.upload_to(b'', done)
 
-                # 下载
-                global speed
+        seconds = time.perf_counter()
+        with tempfile.TemporaryDirectory() as tdir:
+            tdir = Path(tdir)
+            image = tdir / 'image.nii.gz'
+
+            # 下载
+            global speed
+            speed = [time.perf_counter()] * 2 + ['']
+            t = time.perf_counter()
+            ro.download_file(f, image, partial(progress, _ := '下载'))
+            t = time.perf_counter() - t
+            print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
+
+            # 推理
+            for task in cfg['task']:
+                label = tdir / f'{task}.nii.gz'
+                ln = cfg['task'][task].get('license_number')
+
+                print(f'\r[推理] {task}', end='', flush=True)
+                t = time.perf_counter()
+                totalsegmentator(image, label, ml=True, task=task, license_number=ln, quiet=True)
+                t = time.perf_counter() - t
+                print(f'\r[推理] {task} {t:.2f} 秒', flush=True)
+
+                # 上传
+                rw.mkdir(task)
+
                 speed = [time.perf_counter()] * 2 + ['']
                 t = time.perf_counter()
-                ro.download_file(f, image, partial(progress, _ := '下载'))
+                rw.upload_file(_ := f'{task}/{f}', label, partial(progress, _ := '上传'))
                 t = time.perf_counter() - t
                 print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
 
-                # 推理
-                for task in cfg['task']:
-                    label = tdir / f'{task}.nii.gz'
-                    ln = cfg['task'][task].get('license_number')
+            del cfg['undone']
+            cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
 
-                    print(f'\r[推理] {task}', end='', flush=True)
-                    t = time.perf_counter()
-                    totalsegmentator(image, label, ml=True, task=task, license_number=ln, quiet=True)
-                    t = time.perf_counter() - t
-                    print(f'\r[推理] {task} {t:.2f} 秒', flush=True)
+            seconds = time.perf_counter() - seconds
+            print(f'[用时] {seconds:.0f} 秒\n')
 
-                    # 上传
-                    rw.mkdir(task)
-
-                    speed = [time.perf_counter()] * 2 + ['']
-                    t = time.perf_counter()
-                    rw.upload_file(_ := f'{task}/{f}', label, partial(progress, _ := '上传'))
-                    t = time.perf_counter() - t
-                    print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
-
-                del cfg['undone']
-                cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
-
-                seconds = time.perf_counter() - seconds
-                print(f'[用时] {seconds:.0f} 秒\n')
-
-            break
+        break
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', required=True)
+    parser.add_argument('--clean', action='store_true', default=False)
     args = parser.parse_args()
 
-    main(args.config)
+    while True:
+        try:
+            main(args.config, args.clean)
+        except Exception as e:
+            warnings.warn(str(e))
