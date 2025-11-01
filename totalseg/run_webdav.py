d4984d95db869dfb26d9f0835edcde6462537106
epw <88172828@qq.com>
2025-11-02 01:41:00 +0800

    重构了totalseg分布式任务实现

--- totalseg/run_webdav.py
+++ totalseg/run_webdav.py
@@ -2,15 +2,15 @@
 # pip install tomlkit webdavclient3 humanize TotalSegmentator
 
 import argparse
+import os
+import subprocess
 import sys
 import tempfile
 import time
-import warnings
 from functools import partial
 from pathlib import Path
 
 import humanize
-import requests
 import tomlkit
 from totalsegmentator.python_api import totalsegmentator
 from webdav3.client import Client
@@ -23,96 +23,157 @@ def progress(prefix, current, total):
     speed[1] = time.perf_counter()
     _ = float(current) / (speed[1] - speed[0]) if speed[0] != speed[1] else 0.0
     speed[2] = f'{humanize.naturalsize(_, True)}/s'
-    print(f'\r[{prefix}] {speed[2]} {100 * current / total:.2f}%', end='', flush=True)
+    print(f'\r{prefix} {speed[2]} {100 * current / total:.2f}%', end='', flush=True)
 
 
-def main(cfg_path: str, clean: bool):
+def main(cfg_path: str, fs_path: str):
+    global speed
+
+    cfg_path = Path(cfg_path)
+    cfg = tomlkit.loads(cfg_path.read_text('utf-8'))
+
+    fs_path = Path(fs_path)
+    fs = tomlkit.loads(fs_path.read_text('utf-8'))
+
+    todo = fs['todo']
+    fs['done'] = fs.get('done', {})
+
+    with tempfile.TemporaryDirectory() as tdir:
+        for i, f in enumerate(todo):
+            i += 1
+
+            tdir = Path(tdir)
+            image = tdir / f
+
+            for task in cfg['task']:
+                fs['done'][task] = fs['done'].get(task, [])
+
+                if f in fs['done'][task]:
+                    continue
+
+                print(f'[进度] {i}/{len(todo)} {100 * i / len(todo):.3f}% {cfg_path.name} {fs_path.name}')
+                print(f'[任务] {f} {task}')
+
+                seconds = time.perf_counter()
+                label = tdir / f'{task}.nii.gz'
+
+                # 下载
+                if not image.exists():
+                    while True:
+                        if subprocess.call([
+                            sys.executable, 'run_webdav.py',
+                            '--config', args.config,
+                            '--download',
+                            '--remote', f,
+                            '--local', image.as_posix(),
+                        ], cwd=os.getcwd()) == 0:
+                            break
+                        print('[下载] 重试')
+
+                # 推理
+                while True:
+                    if subprocess.call([
+                        sys.executable, 'run_webdav.py',
+                        '--config', args.config,
+                        '--inference',
+                        '--task', task,
+                        '--image', image.as_posix(),
+                        '--label', label.as_posix(),
+                    ], cwd=os.getcwd()) == 0:
+                        break
+                    print('[推理] 重试')
+
+                # 上传
+                while True:
+                    if subprocess.call([
+                        sys.executable, 'run_webdav.py',
+                        '--config', args.config,
+                        '--upload',
+                        '--remote', f'{task}/{f}',
+                        '--local', label.as_posix(),
+                        '--task', task,
+                    ], cwd=os.getcwd()) == 0:
+                        break
+                    print('[上传] 重试')
+
+                fs['done'][task].append(f)
+                fs_path.write_text(tomlkit.dumps(fs), 'utf-8')
+
+                seconds = time.perf_counter() - seconds
+                print(f'[用时] {seconds:.0f} 秒\n')
+
+                label.unlink(True)
+            image.unlink(True)
+
+
+def download(cfg_path: str, remote: str, local: str):
+    global speed
+
     cfg_path = Path(cfg_path)
     cfg = tomlkit.loads(cfg_path.read_text('utf-8'))
 
     ro = Client(cfg['webdav']['images'])
+
+    name = f'[下载] {remote}'
+    print(f'\r{name}', end='', flush=True)
+    speed = [time.perf_counter()] * 2 + ['']
+    t = time.perf_counter()
+    ro.download_file(remote, local, partial(progress, name))
+    t = time.perf_counter() - t
+    print(f'\r{name} {speed[2]} {t:.2f} 秒', flush=True)
+
+
+def inference(cfg_path: str, task: str, image: str, label: str):
+    cfg_path = Path(cfg_path)
+    cfg = tomlkit.loads(cfg_path.read_text('utf-8'))
+    ln = cfg['task'][task].get('license_number')
+
+    print(f'\r[推理] {task}', end='', flush=True)
+    t = time.perf_counter()
+    totalsegmentator(image, label, ml=True, task=task, license_number=ln, quiet=True)
+    t = time.perf_counter() - t
+    print(f'\r[推理] {task} {t:.2f} 秒', flush=True)
+
+
+def upload(cfg_path: str, remote: str, local: str, task: str):
+    global speed
+
+    cfg_path = Path(cfg_path)
+    cfg = tomlkit.loads(cfg_path.read_text('utf-8'))
+
     rw = Client(cfg['webdav']['labels'])
-    rw.mkdir('.done')
 
-    if undone := cfg.get('undone'):
-        for _ in rw.list('.done'):
-            if _ == undone:
-                rw.clean(_ := f'.done/{_}')
-                del cfg['undone']
-                cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
-                print(f'[清理] {_}\n')
+    rw.mkdir(task)
 
-    if clean:
-        sys.exit()
-
-    ro_list = ro.list()
-    rw_list = rw.list('.done')
-    print(f'[完成] {len(rw_list)}/{len(ro_list)} {100 * len(rw_list) / len(ro_list):.3f}%')
-
-    for f in (set(ro_list) - set(rw_list)):
-        done = f'.done/{f}'
-
-        # 跳过完成
-        if requests.head(f'{rw.webdav.hostname}/{done}', auth=(rw.webdav.login, rw.webdav.password)).ok:
-            continue
-
-        print(f'[新建] {f} {cfg_path.name}')
-
-        # 文件锁
-        cfg['undone'] = f
-        cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
-        rw.upload_to(b'', done)
-
-        seconds = time.perf_counter()
-        with tempfile.TemporaryDirectory() as tdir:
-            tdir = Path(tdir)
-            image = tdir / 'image.nii.gz'
-
-            # 下载
-            global speed
-            speed = [time.perf_counter()] * 2 + ['']
-            t = time.perf_counter()
-            ro.download_file(f, image, partial(progress, _ := '下载'))
-            t = time.perf_counter() - t
-            print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
-
-            # 推理
-            for task in cfg['task']:
-                label = tdir / f'{task}.nii.gz'
-                ln = cfg['task'][task].get('license_number')
-
-                print(f'\r[推理] {task}', end='', flush=True)
-                t = time.perf_counter()
-                totalsegmentator(image, label, ml=True, task=task, license_number=ln, quiet=True)
-                t = time.perf_counter() - t
-                print(f'\r[推理] {task} {t:.2f} 秒', flush=True)
-
-                # 上传
-                rw.mkdir(task)
-
-                speed = [time.perf_counter()] * 2 + ['']
-                t = time.perf_counter()
-                rw.upload_file(_ := f'{task}/{f}', label, partial(progress, _ := '上传'))
-                t = time.perf_counter() - t
-                print(f'\r[{_}] {speed[2]} {t:.2f} 秒', flush=True)
-
-            del cfg['undone']
-            cfg_path.write_text(tomlkit.dumps(cfg), 'utf-8')
-
-            seconds = time.perf_counter() - seconds
-            print(f'[用时] {seconds:.0f} 秒\n')
-
-        break
+    name = f'[上传] {remote}'
+    print(f'\r{name}', end='', flush=True)
+    speed = [time.perf_counter()] * 2 + ['']
+    t = time.perf_counter()
+    rw.upload_file(remote, local, partial(progress, name))
+    t = time.perf_counter() - t
+    print(f'\r{name} {speed[2]} {t:.2f} 秒', flush=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', required=True)
+    parser.add_argument('--fs', default='')
     parser.add_argument('--clean', action='store_true', default=False)
+    parser.add_argument('--download', action='store_true', default=False)
+    parser.add_argument('--remote', default='')
+    parser.add_argument('--local', default='')
+    parser.add_argument('--inference', action='store_true', default=False)
+    parser.add_argument('--task', default='')
+    parser.add_argument('--image', default='')
+    parser.add_argument('--label', default='')
+    parser.add_argument('--upload', action='store_true', default=False)
     args = parser.parse_args()
 
-    while True:
-        try:
-            main(args.config, args.clean)
-        except Exception as e:
-            warnings.warn(str(e))
+    if args.download:
+        download(args.config, args.remote, args.local)
+    elif args.inference:
+        inference(args.config, args.task, args.image, args.label)
+    elif args.upload:
+        upload(args.config, args.remote, args.local, args.task)
+    else:
+        main(args.config, args.fs)
